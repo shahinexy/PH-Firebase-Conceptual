14a48df5a2a486f88a78ec3baf911ed4d4e33f13
shahinexy <shahinexy@gmail.com>
2024-04-03 08:16:38 +0600

    emial vailidetion done

--- src/components/Layout/Register.jsx
+++ src/components/Layout/Register.jsx
@@ -1,14 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../provider/AuthProvaider";
 
 const Register = () => {
     const {createUser} = useContext(AuthContext)
+    const [error, setError] = useState('')
 
     const handleRegister = e =>{
         e.preventDefault()
         const name = e.target.name.value;
         const email = e.target.email.value;
         const pass = e.target.password.value;
+        const confirmPass = e.target.confirm.value;
+
+        if(!/@gmail\.com$/.test(email)){
+            setError(`email should be contain '@gmail.com'`)
+            return
+        }
+        if(pass.length < 6){
+            setError('password should at least 6 character or longer')
+            return
+        }
+        if(!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(pass)){
+            setError('password should contain at least one specail character')
+            return
+        }
+        if(pass !== confirmPass){
+            setError('Password did not match')
+            return
+        }
+
+        setError('')
         console.log(name, email, pass);
         createUser(email, pass)
     }
@@ -52,8 +73,23 @@ const Register = () => {
                 
               />
             </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Confirm Password</span>
+              </label>
+              <input
+                type="password"
+                name="confirm"
+                placeholder="password"
+                className="input input-bordered"
+                
+              />
+            </div>
             <div className="form-control mt-6">
               <button className="btn bg-emerald-700 text-white text-lg font-bold">Register</button>
+            </div>
+            <div>
+                {error && <small className="text-red-500">{error}</small>}
             </div>
           </form>
         </div>
